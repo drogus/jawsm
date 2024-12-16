f2e4a35966a8138a80f51b12735178a05a9c8ec6
Piotr Sarnacki <drogus@gmail.com>
2024-12-17 00:50:28 +0100

    feat: Add script to compare test results between two JSON files

---
+++ compare_results.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+if ARGV.length != 2
+  puts "Usage: #{$0} <first_results.json> <second_results.json>"
+  exit 1
+end
+
+first_file, second_file = ARGV
+
+begin
+  first_results = JSON.parse(File.read(first_file))
+  second_results = JSON.parse(File.read(second_file))
+rescue JSON::ParserError => e
+  puts "Error parsing JSON files: #{e.message}"
+  exit 1
+rescue Errno::ENOENT => e
+  puts "Error reading files: #{e.message}"
+  exit 1
+end
+
+# Create lookup hash for second results
+second_lookup = second_results.each_with_object({}) do |result, hash|
+  hash[result['file']] = result['pass']
+end
+
+# Find tests that passed in first but failed in second
+regressions = first_results.select do |result|
+  result['pass'] && second_lookup.key?(result['file']) && !second_lookup[result['file']]
+end
+
+if regressions.empty?
+  puts "No regressions found!"
+else
+  puts "Tests that passed in #{first_file} but failed in #{second_file}:"
+  regressions.each do |result|
+    puts result['file']
+  end
+  puts "\nTotal regressions: #{regressions.length}"
+end
